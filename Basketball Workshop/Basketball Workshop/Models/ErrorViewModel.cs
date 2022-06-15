26525c68cce68d9466c961b19175ff0766702bdc
daviswcci <79462946+daviswcci@users.noreply.github.com>
2022-06-15 13:27:37 -0400

    init

---
+++ Basketball Workshop/Basketball Workshop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Basketball_Workshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
