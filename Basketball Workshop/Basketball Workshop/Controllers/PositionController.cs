d3556c5f176fbd1db82d1b0c7febb453c01b9f0a
Gavin <gavin@wecancodeit.org>
2022-06-17 11:06:48 -0400

    Created position controller and view

---
+++ Basketball Workshop/Basketball Workshop/Controllers/PositionController.cs
@@ -0,0 +1,14 @@
+﻿using Basketball_Workshop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basketball_Workshop.Controllers
+{
+    public class PositionController : Controller
+    {
+        private List<Position> db = new List<Position>() { new Position() { Name = "Point Guard" }, new Position() { Name = "Shooting Guard" }, new Position() { Name = "Center" } };
+        public IActionResult Index()
+        {
+            return View(db);
+        }
+    }
+}
--- Basketball Workshop/Basketball Workshop/Models/Position.cs
+++ Basketball Workshop/Basketball Workshop/Models/Position.cs
@@ -10,5 +10,10 @@
             Name = name;
             Players = players;
         }
+
+        public Position()
+        {
+
+        }
     }
 }
